1e7401ed5a8d567ae67b772627f208254b1a964a
Kevin McPhillips <github@kevinmcphillips.ca>
2013-07-01 11:06:01 -0500

    0.4.0: Update to rails 4

--- spec/lib/acts_as_permalink_spec.rb
+++ spec/lib/acts_as_permalink_spec.rb
@@ -2,9 +2,8 @@ require 'spec_helper'
 
 describe Acts::Permalink do
   describe "default attributes" do
-    
+
     class Post < ActiveRecord::Base
-      attr_accessible :title, :permalink
       acts_as_permalink
     end
 
@@ -29,9 +28,8 @@ describe Acts::Permalink do
   end
 
   describe "single table inheritance" do
-    
+
     class Thing < ActiveRecord::Base
-      attr_accessible :title, :permalink
       acts_as_permalink
     end
 
@@ -48,7 +46,6 @@ describe Acts::Permalink do
   describe "custom attributes" do
     class OtherPost < ActiveRecord::Base
       self.table_name = "posts"
-      attr_accessible :other_title, :other_permalink
       acts_as_permalink :to => :other_permalink, :from => :other_title
     end
 
