63c71352197f2cca7d2a0d34fe866eeed8540f35
Kevin McPhillips <github@kevinmcphillips.ca>
2015-10-26 17:17:13 -0400

    Add to_permalink to String

---
+++ spec/lib/string_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe String do
+  describe "#to_permalink" do
+    it "converts a string to a permalink" do
+      expect("it's a wonderful day!!".to_permalink).to eq("it-s-a-wonderful-day")
+      expect("@$#a{$%^b#$%#%c!@<>d,./".to_permalink).to eq("a-b-c-d")
+    end
+
+    it "allows overriding the separator" do
+      expect("(Oh hey neat), an underscore __ character".to_permalink(separator: "_")).to eq("oh_hey_neat_an_underscore_character")
+    end
+
+    it "allows defining a max length" do
+      expect("_-_-_-123456_-_-_-_".to_permalink(max_length: 3)).to eq("123")
+    end
+  end
+end
